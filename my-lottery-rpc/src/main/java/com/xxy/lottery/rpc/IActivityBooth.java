6be94b31aad15235ff66a61407e7028d0b05879d
chelsea0111 <xuxinyi9977@gmail.com>
2024-01-09 00:23:48 +0800

    02-buildFramework

--- my-lottery-rpc/src/main/java/com/xxy/lottery/rpc/IActivityBooth.java
+++ my-lottery-rpc/src/main/java/com/xxy/lottery/rpc/IActivityBooth.java
@@ -3,6 +3,11 @@ package com.xxy.lottery.rpc;
 import com.xxy.lottery.rpc.req.ActivityReq;
 import com.xxy.lottery.rpc.res.ActivityRes;
 
+/**
+ * @author xuxinyi
+ * @date 2024/1/8 23:46
+ * @description 提供给外部的接口描述
+*/
 public interface IActivityBooth {
     ActivityRes queryActivityById(ActivityReq req);
 }
--- my-lottery-rpc/src/main/java/com/xxy/lottery/rpc/res/ActivityRes.java
+++ my-lottery-rpc/src/main/java/com/xxy/lottery/rpc/res/ActivityRes.java
@@ -2,21 +2,18 @@ package com.xxy.lottery.rpc.res;
 
 import com.xxy.lottery.common.Result;
 import com.xxy.lottery.rpc.dto.ActivityDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ActivityRes implements Serializable {
 
     private Result result;
     private ActivityDto activity;
-
-    public ActivityRes(Result result) {
-        this.result = result;
-    }
-
-    public ActivityRes(Result result, ActivityDto activity) {
-        this.result = result;
-        this.activity = activity;
-    }
 }
